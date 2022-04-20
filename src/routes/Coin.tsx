eb1e6302f88b6ace1fdf21d8654ae79f307ef292
UHyun-K <konglim2@naver.com>
2022-04-20 15:04:56 +0900

    data types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
@@ -26,15 +26,86 @@ const Loader = styled.span`
     display: center;
 `;
 
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    team: object;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    whitepaper: object;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        };
+    };
+}
+
 function Coin() {
     const [loading, setLoading] = useState(true);
     const { coinId } = useParams();
+    const state = useLocation().state as RouterState;
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
 
     interface RouterState {
         name: string;
     }
-    const state = useLocation().state as RouterState;
-    console.log(state);
+
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            console.log(infoData);
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json();
+            console.log(priceData);
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, []);
 
     return (
         <Container>
